88ab93462fcc8014d937c9961b977d6739c15349
DianeU <diane.uschner@gmail.com>
2020-08-12 08:58:07 -0400

    Evaluation of the simulation results

---
+++ simulations/02_scripts/create-figures.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(ggsci)
+D <- read_rds("../simulations/03_results/2020-08-12_confidence-intervals.Rds")
+D <- as_tibble(lapply(D, unlist))
+
+D <- D %>%
+  mutate(bias = trueP - xi,
+         width = UL - LL,
+         inside = trueP >= LL & trueP <= UL
+  )
+
+results <- D %>%
+  group_by(K, N, method, trueP) %>%
+  summarize(
+    bias_exp = mean(bias),
+    bias_sd = sd(bias),
+    width_exp = mean(width),
+    width_sd = sd(width),
+    cov_prob = sum(inside)/n()*100,
+    time_exp = mean(time),
+    time_sd = sd(time)
+  )
+
+
+# Estimation of the bias does not depend on the CI
+D %>%
+  filter(method == "bootstrap") %>%
+  ggplot(aes(as.factor(trueP), bias, fill = as.factor(N))) + geom_boxplot() + theme_minimal() +
+  theme(legend.position = "top") +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800", "#FC4E07")) +
+  labs(x = "True DOOR probability", fill = "Sample size", y = "Bias (true - estimated)")
+ggsave(paste0("../simulations/03_results/", Sys.Date(), "_bias.png"))
+
+results %>%
+  #filter(N == 100) %>%
+  ggplot(aes(trueP, cov_prob, color = as.factor(method))) +
+  geom_hline(yintercept = 95, color = "darkgrey", linetype = "dashed") +
+  geom_point(size = 1.3) + geom_line(size = 1.05) + theme_minimal() +
+  scale_color_jco() +
+  theme(legend.position = "top") + facet_wrap(~N) +
+  labs(x = "True DOOR probability", color = "Method", y = "Coverage Probability") +
+  scale_x_continuous(breaks = c(0.5, 0.66, 0.74), limits=c(0.5, 0.8))
+ggsave(paste0("../simulations/03_results/", Sys.Date(), "_coverage-probability.png"))
+
+
+D %>%
+  filter(trueP == 0.5) %>%
+  ggplot(aes(as.factor(N), width, fill = method)) + geom_boxplot() + theme_minimal() +
+  theme(legend.position = "top") +
+  scale_fill_jco() +
+  labs(x = "Sample size", fill = "Method", y = "Interval Width")
+ggsave(paste0("../simulations/03_results/", Sys.Date(), "_interval-width-by-sample-size_P05.png"))
+
+
+D %>%
+  filter(N == 200) %>%
+  ggplot(aes(as.factor(trueP), width, fill = method)) + geom_boxplot() + theme_minimal() +
+  theme(legend.position = "top") +
+  scale_fill_jco() +
+  labs(x = "True DOOR Probability", fill = "Method", y = "Interval Width")
+ggsave(paste0("../simulations/03_results/", Sys.Date(), "_interval-width-by-true-prob_N200.png"))
+
+
+
+timings <- results %>%
+  ungroup() %>%
+  mutate(time_exp = round(time_exp, 4)) %>%
+  select(N, trueP, method, time_exp) %>%
+  pivot_wider(names_from = "method", values_from = "time_exp")
+write.csv(timings, file = paste0("../simulations/03_results/", Sys.Date(), "_simulation-timing.csv"))
+
+xtable::xtable(timings, digits = 4)
--- simulations/02_scripts/start_sim.R
+++ simulations/02_scripts/start_sim.R
@@ -6,7 +6,7 @@ set.seed(123)
 alpha <- 0.05
 
 #Number of simulation repetitions r = 10,000 (for a start)
-r <- 5
+r <- 1000
 
 # Two Treatment Arms A,B
 tx <- c("A", "B")
@@ -18,23 +18,37 @@ K <- 3
 pA <- c(0.2, 0.2, 0.6)
 pB <- c(0.2, 0.6, 0.2)
 
+trueP <- c(0.5, 0.66, 0.74)
+
 #Sample size N = 100,200,500.
-N <- 100
+N <- c(100,200,500)
 
 # Confidence-Interval Method "\"bootstrap\",\"multinom\" }"
 # For "method == bootstrap" , number of bootstrap samples B= 1500
-# method <- c("bootstrap", "multinom", "halperin")
-method <- c("multinom", "halperin")
-B <- 1500
+ method <- c("bootstrap", "multinom", "halperin")
+#method <- c("multinom", "halperin")
+B <- 500
 
-par <- expand.grid(alpha = alpha, r=r, K = K, N = N, method = method, B = B)
+par <- expand.grid(alpha = alpha, r=r, K = K, N = N, method = method, B = B, trueP = trueP, stringsAsFactors = FALSE)
 
-D <- data.frame(alpha = numeric(0), r = numeric(0), K = numeric(0), N = numeric(0), method = character(0), B = numeric(0),
+D <- data.frame(alpha = numeric(0), r = numeric(0), K = numeric(0), N = numeric(0), method = character(0), B = numeric(0), trueP = numeric(0),
                 xi = numeric(0), LL = numeric(0), UL = numeric(0), time = numeric(0))
 
 getData <- function(x){
   N <- x$N
   myData <- data.frame(seq = rep(tx, each = N/2))
+
+  if(x$trueP == 0.5){
+    pA <- c(0.2, 0.2, 0.6)
+    pB <- c(0.2, 0.2, 0.6)
+  } else if (x$trueP == 0.66) {
+    pA <- c(0.2, 0.2, 0.6)
+    pB <- c(0.2, 0.6, 0.2)
+  } else if (x$trueP == 0.74) {
+    pA <- c(0.2, 0.2, 0.6)
+    pB <- c(0.2, 0.6, 0.2)
+  }
+
   myData <- myData %>%
     mutate(DOOR = (seq==tx[1])* sample(1:(x$K), size = N, replace = TRUE, prob = pA) +
              (seq==tx[2])* sample(1:(x$K), size = N, replace = TRUE, prob = pB))
@@ -44,13 +58,15 @@ getData <- function(x){
   ci <- get_door_prob_CI(res, tx = c("A", "B"), method = x$method)
   t2 <- Sys.time()
   time <- t2 - t1
-  c(x, xi, ci, time)
+  c(x, xi = xi, LL = ci[1], UL = ci[2], time = as.numeric(time))
 }
 
+T1 <- Sys.time()
 for (i in 1:nrow(par)) {
   x <- par[i, , drop = FALSE]
   tmp <- t(replicate(x$r, getData(x)))
   D <- rbind(D, tmp)
 }
+T2<- Sys.time()
 
 write_rds(D, path = paste0("../simulations/03_results/", Sys.Date(), "_confidence-intervals.Rds"))
