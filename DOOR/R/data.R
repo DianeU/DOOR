5863261bc2f4d075795e97b6be6e3f63b6e198c5
DianeU <diane.uschner@gmail.com>
2020-08-15 12:09:05 -0400

    Added data, example code, and vignette

---
+++ DOOR/R/data.R
@@ -0,0 +1,24 @@
+#' DOOR Sample Data (N = 26)
+#'
+#' An artificial data set containing the the treatment assignment
+#' vector and the DOOR outcome.
+#'
+#' @format A data frame with 26 rows and 2 variables:
+#' \describe{
+#'   \item{seq}{treament assignment sequence}
+#'   \item{DOOR}{DOOR outcome with four categories}
+#' }
+"sample_data"
+
+
+#' DOOR Sample Data (N = 300)
+#'
+#' An artificial data set containing the the treatment assignment
+#' vector and the DOOR outcome.
+#'
+#' @format A data frame with 300 rows and 2 variables:
+#' \describe{
+#'   \item{seq}{treament assignment sequence}
+#'   \item{DOOR}{DOOR outcome with four categories}
+#' }
+"sample_data_N300"
